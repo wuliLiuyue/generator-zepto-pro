02d4d3ca4eeb88ebd5f77b9e9df7372b7546bb86
成兴 <liaochengxing@kaike.la>
2018-07-11 10:43:20 +0800

    typescript配置添加

---
+++ config/webpack.conf.js
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////
+///                                                          ///
+///                   webpack打包配置                         ///
+///                   ts编译                                 ///
+///                                                          ///
+////////////////////////////////////////////////////////////////
+
+const path = require('path');
+const webpack = require('webpack');
+
+/**
+ * 基础配置
+ */
+
+const BaseConf = {
+    mode: 'development',
+    cache: true,
+    devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [{
+                    loader: 'ts-loader'
+                }],
+                include: [ path.resolve('generators') ]
+            }
+        ]
+    }
+};
+
+/**
+ * 打包配置
+ */
+
+module.exports = [Object.assign({}, BaseConf, {
+    entry: {
+        app: path.join(__dirname, '../generators/app/index.ts')
+    },
+    output: {
+        path: path.join(__dirname, `../generators/app`),
+        filename: 'index.js'
+    }
+})];

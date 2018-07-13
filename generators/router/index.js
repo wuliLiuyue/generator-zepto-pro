b7f8c0ac3f06547b78468b07cf8f1719a6c2b94c
成兴 <liaochengxing@kaike.la>
2018-07-13 10:36:13 +0800

    figlet异常处理

---
+++ generators/router/index.js
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////
+///                                                          ///
+///                   yeoman子项目构建逻辑                       ///
+///                   继承自yeoman-generator类                ///
+///                   在生命周期回调中注入动态逻辑               ///
+///                                                          ///
+////////////////////////////////////////////////////////////////
+
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk');
+const generators = require('yeoman-generator');
+const figlet = require('figlet');
+const ora = require('ora');
+
+module.exports = class router extends generators {
+  /**
+   * 继承yeoman-generator
+   * 获取生命周期hook
+   */
+
+  constructor(args, opts) {
+    super(args, opts);
+  };
+
+  /**
+   * 脚手架初始化
+   */
+
+  initializing() {
+    console.log('test');
+  };
+};
